9dfe7c8ae338b209b8fa604aac7c8f9c02e66f42
Stanislav Doroshin <stanislav.doroshin@bsc-ideas.com>
2017-07-26 20:02:34 +0300

    Переработан VelocityResponseTransformer. Настройки Velocity вынесены в отдельный файл. Шаблоны хранятся снаружи проекта.

--- src/main/java/ru/bsc/wiremock/transformers/CustomWireMockWebContextListener.java
+++ src/main/java/ru/bsc/wiremock/transformers/CustomWireMockWebContextListener.java
@@ -1,24 +1,26 @@
 package ru.bsc.wiremock.transformers;
 
-import com.github.adamyork.wiremock.transformer.VelocityResponseTransformer;
 import com.github.tomakehurst.wiremock.common.Notifier;
 import com.github.tomakehurst.wiremock.common.Slf4jNotifier;
 import com.github.tomakehurst.wiremock.core.WireMockApp;
 import com.github.tomakehurst.wiremock.extension.Extension;
 import com.github.tomakehurst.wiremock.extension.ResponseDefinitionTransformer;
-import com.github.tomakehurst.wiremock.extension.responsetemplating.ResponseTemplateTransformer;
 import com.github.tomakehurst.wiremock.http.AdminRequestHandler;
 import com.github.tomakehurst.wiremock.http.StubRequestHandler;
 import com.github.tomakehurst.wiremock.servlet.NotImplementedContainer;
 import com.github.tomakehurst.wiremock.servlet.WarConfiguration;
 import com.github.tomakehurst.wiremock.servlet.WireMockWebContextListener;
 import org.apache.velocity.app.Velocity;
+import org.apache.velocity.runtime.RuntimeConstants;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 
 import static com.google.common.base.MoreObjects.firstNonNull;
 
@@ -58,9 +60,17 @@ public class CustomWireMockWebContextListener extends WireMockWebContextListener
                 Map<String, T> transformers = new HashMap<>();
                 //transformers.put("soap-body-transformer", (T) new SoapBodyTransformer());
                 //transformers.put("response-template", (T) new ResponseTemplateTransformer(true));
-                Velocity.setProperty("resource.loader", "class");
-                Velocity.setProperty("class.resource.loader.class", "org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader");
-                transformers.put("velocity-response-transformer", (T) new VelocityResponseTransformer());
+
+                // VelocityResponseTransformer configuration
+                Properties properties = new Properties();
+                try (final InputStream stream = this.getClass().getResourceAsStream("/velocity.properties")) {
+                    properties.load(stream);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                Velocity.init(properties);
+
+                transformers.put("velocity-response-transformer", (T) new CustomVelocityResponseTransformer());
                 return transformers;
             }
             return Collections.emptyMap();
---
+++ src/main/java/ru/bsc/wiremock/velocity/directive/XPathDirective.java
@@ -0,0 +1,49 @@
+package ru.bsc.wiremock.velocity.directive;
+
+import org.apache.velocity.context.InternalContextAdapter;
+import org.apache.velocity.exception.MethodInvocationException;
+import org.apache.velocity.exception.ParseErrorException;
+import org.apache.velocity.exception.ResourceNotFoundException;
+import org.apache.velocity.runtime.directive.Directive;
+import org.apache.velocity.runtime.parser.node.Node;
+import org.xml.sax.InputSource;
+
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+import java.io.IOException;
+import java.io.StringReader;
+import java.io.Writer;
+
+/**
+ * Created by sdoroshin on 26.07.2017.
+ *
+ */
+public class XPathDirective extends Directive {
+
+    @Override
+    public String getName() {
+        return "xpath";
+    }
+
+    @Override
+    public int getType() {
+        return LINE;
+    }
+
+    @Override
+    public boolean render(InternalContextAdapter context, Writer writer, Node node ) throws IOException, ResourceNotFoundException, ParseErrorException, MethodInvocationException {
+        String variableName = node.jjtGetChild(0).getFirstToken().image.substring(1);
+        String xmlDocument = String.valueOf(node.jjtGetChild(1).value(context));
+        String xpathString = String.valueOf(node.jjtGetChild(2).value(context));
+
+        XPath xpath = XPathFactory.newInstance().newXPath();
+        try {
+            String evaluatedValue = xpath.evaluate(xpathString, new InputSource(new StringReader(xmlDocument)));
+            context.put(variableName, evaluatedValue);
+        } catch (XPathExpressionException e) {
+            throw new IOException( "cannot evaluate xpath: " + e.getMessage() );
+        }
+        return true;
+    }
+}
