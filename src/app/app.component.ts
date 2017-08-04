259cfdaa2ca28e7f3ab36dc7fc4c5082c0e9067d
Stanislav Doroshin <stanislav.doroshin@bsc-ideas.com>
2017-08-04 16:26:17 +0300

    Сохранение всех маппингов на диск.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {WireMockService} from '../service/wire-mock.service';
 import {Mapping} from '../model/mapping';
+import {ToastOptions, ToastyService} from 'ng2-toasty';
 
 @Component({
   selector: 'app-root',
@@ -10,10 +11,28 @@ import {Mapping} from '../model/mapping';
 export class AppComponent implements OnInit {
   mappingList: Mapping[];
 
-  constructor(private wireMockService: WireMockService) { }
+  constructor(
+    private wireMockService: WireMockService,
+    private toastyService: ToastyService
+  ) { }
 
   ngOnInit(): void {
     this.wireMockService.getMappingList()
       .then(mappingList => this.mappingList = mappingList);
+  }
+
+  saveToBackStorage() {
+    if (confirm('Confirm saving')) {
+      this.wireMockService.saveToBackStorage().then(() => {
+        const toastOptions: ToastOptions = {
+          title: 'Saved',
+          msg: 'Маппинги сохранены на диск',
+          showClose: true,
+          timeout: 5000,
+          theme: 'bootstrap'
+        };
+        this.toastyService.success(toastOptions)
+      });
+    }
   }
 }
