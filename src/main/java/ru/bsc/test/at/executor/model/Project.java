f525106c2ae032e375b398cfb2302a2770c25b94
Stanislav Doroshin <stanislav.doroshin@bsc-ideas.com>
2017-09-15 19:06:08 +0300

    Удаление стенда из проекта.

--- src/main/java/ru/bsc/test/at/executor/model/Project.java
+++ src/main/java/ru/bsc/test/at/executor/model/Project.java
@@ -60,7 +60,7 @@ public class Project implements Serializable, Cloneable {
     @JsonManagedReference
     private List<ScenarioGroup> scenarioGroups;
 
-    @OneToMany(mappedBy = "project", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "project", cascade = CascadeType.ALL, orphanRemoval = true)
     @JsonManagedReference
     private List<Stand> standList;
 
