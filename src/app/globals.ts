f8b1dae96b8ec7beef0fd1d8cd77b677c06b9de9
Stanislav Doroshin <stanislav.doroshin@bsc-ideas.com>
2017-10-23 16:53:44 +0300

    Базовый урл сервиса переключен на мастер

--- src/app/globals.ts
+++ src/app/globals.ts
@@ -2,6 +2,5 @@ import {Injectable} from '@angular/core';
 
 @Injectable()
 export class Globals {
-  serviceBaseUrl = '/autotester-rest';
-  // serviceBaseUrl = '/api';
+  serviceBaseUrl = '/autotester';
 }
