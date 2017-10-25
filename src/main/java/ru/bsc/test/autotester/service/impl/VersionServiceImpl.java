3b821f6224308d825bf53d642e875ed4091e449a
Stanislav Doroshin <stanislav.doroshin@bsc-ideas.com>
2017-10-25 11:04:16 +0300

    Версия билда сохраняется мавеном в version.properties. Сервис читает версию и дату сборки из version.properties.

--- src/main/java/ru/bsc/test/autotester/service/impl/VersionServiceImpl.java
+++ src/main/java/ru/bsc/test/autotester/service/impl/VersionServiceImpl.java
@@ -1,23 +1,18 @@
 package ru.bsc.test.autotester.service.impl;
 
 import org.apache.commons.lang3.StringUtils;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.stereotype.Service;
 import ru.bsc.test.autotester.service.VersionService;
 
-import java.util.Map;
-import java.util.Optional;
-import java.util.jar.Attributes;
-import java.util.jar.Manifest;
+import java.util.Properties;
 
 /**
  * @author Pavel Golovkin
  */
 @Service
 public class VersionServiceImpl implements VersionService {
-	private static final String BUILD_IMPL_VERSION = "Implementation-Version";
-	private static final String BUILD_DATE = "Implementation-Build-Date";
+	private static final String BUILD_IMPL_VERSION = "buildnumber";
+	private static final String BUILD_DATE = "builddate";
 
 	@Override
 	public Version getVersion() {
@@ -26,39 +21,17 @@ public class VersionServiceImpl implements VersionService {
 
 	private Version findVersion() {
 		try {
-			Optional<Version> version;
-			Resource[] resources = new PathMatchingResourcePatternResolver()
-					.getResources("classpath*:META-INF/MANIFEST.MF");
-			for (Resource resource : resources) {
-				Manifest mf = new Manifest(resource.getInputStream());
-				version = findVersionInManifest(mf);
-				if (version.isPresent()) {
-					return version.get();
-				}
+			Properties prop = new Properties();
+			prop.load(VersionServiceImpl.class.getClassLoader().getResourceAsStream("version.properties"));
+
+			String implVer = prop.getProperty(BUILD_IMPL_VERSION);
+			String dateStr = prop.getProperty(BUILD_DATE);
+			if (StringUtils.isNotBlank(implVer) && StringUtils.isNotBlank(dateStr)) {
+				return new Version(implVer, dateStr);
 			}
 		} catch (Exception e) {
 			//TODO LOGGING
 		}
 		return new Version(Version.UNKNOWN, Version.UNKNOWN);
-	}
-
-	private Optional<Version> findVersionInManifest(Manifest manifest) {
-		String implVer = findKey(manifest, BUILD_IMPL_VERSION);
-		String dateStr = findKey(manifest, BUILD_DATE);
-		if (StringUtils.isNotBlank(implVer) && StringUtils.isNotBlank(dateStr)) {
-			return Optional.of(new Version(implVer, dateStr));
-		} else {
-			return Optional.empty();
-		}
-	}
-
-	private String findKey(Manifest manifest, String key) {
-		for (Map.Entry<String, Attributes> entry : manifest.getEntries().entrySet()) {
-			String value = entry.getValue().getValue(key);
-			if (value != null) {
-				return value;
-			}
-		}
-		return null;
 	}
 }
