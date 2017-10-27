08ddfbb20b2a3226a8acd46e02d0797d2de8d572
Valeria Bolshakova <valeria.bolshakova@bsc-ideas.com>
2017-10-27 17:12:11 +0300

    COM-27: ignore part of json

--- src/main/java/ru/bsc/test/at/executor/validation/IgnoringComparator.java
+++ src/main/java/ru/bsc/test/at/executor/validation/IgnoringComparator.java
@@ -11,13 +11,31 @@ import org.skyscreamer.jsonassert.comparator.DefaultComparator;
  */
 public class IgnoringComparator extends DefaultComparator {
 
+    private static final String IGNORE = "*ignore*";
+
     public IgnoringComparator(JSONCompareMode mode) {
         super(mode);
     }
 
     @Override
     public void compareValues(String prefix, Object expectedValue, Object actualValue, JSONCompareResult result) throws JSONException {
-        if (!expectedValue.equals("*ignore*"))
-            super.compareValues(prefix, expectedValue, actualValue, result);
+        String expected = expectedValue.toString();
+        String actual = actualValue.toString();
+        if (expected.contains(IGNORE))
+            expected = replaceString(expected, actual);
+            super.compareValues(prefix, expected, actual, result);
+    }
+
+    private String replaceString(String expected, String actual) {
+        StringBuilder expectedSb = new StringBuilder(expected);
+        StringBuilder actualSb = new StringBuilder(actual);
+        int start = expectedSb.indexOf(IGNORE);
+        int end = start + IGNORE.length();
+        String replace = actualSb.substring(start, actualSb.indexOf("<", start));
+        String result = expectedSb.replace(start, end, replace).toString();
+        if (!result.contains(IGNORE)) {
+            return result;
+        }
+        return replaceString(result, actualSb.toString());
     }
 }
