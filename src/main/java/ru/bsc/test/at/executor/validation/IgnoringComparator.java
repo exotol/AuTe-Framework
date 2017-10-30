db98dc1e804092bf17f08fb4c68a4984dd4d0e0c
Valeria Bolshakova <valeria.bolshakova@bsc-ideas.com>
2017-10-30 14:40:41 +0300

    COM-27: ignore part of json

--- src/main/java/ru/bsc/test/at/executor/validation/IgnoringComparator.java
+++ src/main/java/ru/bsc/test/at/executor/validation/IgnoringComparator.java
@@ -1,6 +1,8 @@
 package ru.bsc.test.at.executor.validation;
 
+import org.apache.commons.lang3.StringUtils;
 import org.json.JSONException;
+import org.json.JSONObject;
 import org.skyscreamer.jsonassert.JSONCompareMode;
 import org.skyscreamer.jsonassert.JSONCompareResult;
 import org.skyscreamer.jsonassert.comparator.DefaultComparator;
@@ -11,31 +13,38 @@ import org.skyscreamer.jsonassert.comparator.DefaultComparator;
  */
 public class IgnoringComparator extends DefaultComparator {
 
-    private static final String IGNORE = "*ignore*";
-
     public IgnoringComparator(JSONCompareMode mode) {
         super(mode);
     }
 
     @Override
     public void compareValues(String prefix, Object expectedValue, Object actualValue, JSONCompareResult result) throws JSONException {
-        String expected = expectedValue.toString();
-        String actual = actualValue.toString();
-        if (expected.contains(IGNORE))
-            expected = replaceString(expected, actual);
-            super.compareValues(prefix, expected, actual, result);
+        if (!expectedValue.equals("*ignore*")) {
+            if (expectedValue instanceof JSONObject && actualValue instanceof JSONObject ||
+                    expectedValue instanceof String && actualValue instanceof String) {
+                String expected = expectedValue.toString();
+                String actual = actualValue.toString();
+                String[] expectedParts = expected.split("\\*ignore\\*");
+                if (!containsValues(expectedParts, actual)) {
+                    result.fail(prefix, expectedValue, actualValue);
+                }
+            }
+            else {
+                super.compareValues(prefix, expectedValue, actualValue, result);
+            }
+        }
     }
 
-    private String replaceString(String expected, String actual) {
-        StringBuilder expectedSb = new StringBuilder(expected);
-        StringBuilder actualSb = new StringBuilder(actual);
-        int start = expectedSb.indexOf(IGNORE);
-        int end = start + IGNORE.length();
-        String replace = actualSb.substring(start, actualSb.indexOf("<", start));
-        String result = expectedSb.replace(start, end, replace).toString();
-        if (!result.contains(IGNORE)) {
-            return result;
+    private boolean containsValues(String[] values, String actual) {
+        boolean result = true;
+        for (String value : values) {
+            if (actual.contains(value)) {
+                StringUtils.replaceOnce(actual, value, "");
+            }
+            else {
+                result = false;
+            }
         }
-        return replaceString(result, actualSb.toString());
+        return result;
     }
 }
