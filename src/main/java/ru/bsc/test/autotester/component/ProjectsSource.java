101495999ac25d3c522cbbfe4de627828545f904
Stanislav Doroshin <stanislav.doroshin@bsc-ideas.com>
2017-10-31 19:18:46 +0300

    Сортировка объектов при сохранении и чтении из yaml согласно полю sort.

--- src/main/java/ru/bsc/test/autotester/component/ProjectsSource.java
+++ src/main/java/ru/bsc/test/autotester/component/ProjectsSource.java
@@ -3,7 +3,13 @@ package ru.bsc.test.autotester.component;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import org.yaml.snakeyaml.DumperOptions;
 import org.yaml.snakeyaml.Yaml;
+import org.yaml.snakeyaml.nodes.MappingNode;
+import org.yaml.snakeyaml.nodes.Node;
+import org.yaml.snakeyaml.nodes.NodeTuple;
+import org.yaml.snakeyaml.nodes.ScalarNode;
+import org.yaml.snakeyaml.serializer.AnchorGenerator;
 import ru.bsc.test.at.executor.model.AbstractModel;
 import ru.bsc.test.at.executor.model.Project;
 
@@ -67,6 +73,7 @@ public class ProjectsSource {
     }
 
     public void save() {
+        checkAndRepairSort();
         checkAndRepairId();
         saveToFiles();
     }
@@ -75,7 +82,9 @@ public class ProjectsSource {
         projectList.forEach(projectItem -> {
             try {
                 FileWriter writer = new FileWriter(directoryPath + "/" + projectItem.getProjectCode() + "/" + MAIN_YAML_FILENAME);
-                new Yaml().dump(projectItem, writer);
+                DumperOptions dumperOptions = new DumperOptions();
+                dumperOptions.setAnchorGenerator(new AutotesterAnchorGenerator());
+                new Yaml(dumperOptions).dump(projectItem, writer);
                 writer.flush();
                 writer.close();
             } catch (IOException e) {
@@ -84,10 +93,16 @@ public class ProjectsSource {
         });
     }
 
+    private void checkAndRepairSort() {
+        applyToAllModels(
+                model -> {},
+                modelList -> modelList.sort((o1, o2) -> Long.compare(o1.getSort() != null ? o1.getSort() : 0L, o2.getSort() != null ? o2.getSort() : 0L)));
+    }
+
     private void checkAndRepairId() {
         Set<Long> idSet = new LinkedHashSet<>();
-        applyToAllModels(model -> collectAndReplaceRepeatsId(idSet, model));
-        applyToAllModels(model -> newIdToModel(idSet, model));
+        applyToAllModels(model -> collectAndReplaceRepeatsId(idSet, model), modelList -> {});
+        applyToAllModels(model -> newIdToModel(idSet, model), modelList -> {});
     }
 
     private void collectAndReplaceRepeatsId(Set<Long> idSet, AbstractModel model) {
@@ -113,27 +128,33 @@ public class ProjectsSource {
         }
     }
 
-    private void applyToAllModels(IMethodToModel methodToModel) {
+    private void applyToAllModels(IMethodToModel methodToModel, IMethodToList methodToList) {
         projectList.forEach(project -> {
             methodToModel.method(project);
             if (project.getScenarios() != null) {
+                methodToList.method(project.getScenarios());
                 project.getScenarios().forEach(scenario -> {
                     methodToModel.method(scenario);
                     if (scenario.getSteps() != null) {
+                        methodToList.method(scenario.getSteps());
                         scenario.getSteps().forEach(step -> {
                             methodToModel.method(step);
                             if (step.getExpectedServiceRequests() != null) {
+                                methodToList.method(step.getExpectedServiceRequests());
                                 step.getExpectedServiceRequests().forEach(methodToModel::method);
                             }
                             if (step.getStepParameterSetList() != null) {
+                                methodToList.method(step.getStepParameterSetList());
                                 step.getStepParameterSetList().forEach(stepParameterSet -> {
                                     methodToModel.method(stepParameterSet);
                                     if (stepParameterSet.getStepParameterList() != null) {
+                                        methodToList.method(stepParameterSet.getStepParameterList());
                                         stepParameterSet.getStepParameterList().forEach(methodToModel::method);
                                     }
                                 });
                             }
                             if (step.getMockServiceResponseList() != null) {
+                                methodToList.method(step.getMockServiceResponseList());
                                 step.getMockServiceResponseList().forEach(methodToModel::method);
                             }
                         });
@@ -141,9 +162,11 @@ public class ProjectsSource {
                 });
             }
             if (project.getScenarioGroups() != null) {
+                methodToList.method(project.getScenarioGroups());
                 project.getScenarioGroups().forEach(methodToModel::method);
             }
             if (project.getStandList() != null) {
+                methodToList.method(project.getStandList());
                 project.getStandList().forEach(methodToModel::method);
             }
         });
@@ -152,5 +175,34 @@ public class ProjectsSource {
     @FunctionalInterface
     interface IMethodToModel {
         void method(AbstractModel model);
+    }
+
+    @FunctionalInterface
+    interface IMethodToList {
+        void method(List<? extends AbstractModel> modelList);
+    }
+
+    private class AutotesterAnchorGenerator implements AnchorGenerator {
+
+        private long lastAnchorId = 0;
+
+        @Override
+        public String nextAnchor(Node node) {
+            if (node instanceof MappingNode) {
+                NodeTuple idNode = ((MappingNode) node).getValue()
+                        .stream()
+                        .filter(nodeTuple -> nodeTuple.getKeyNode() instanceof ScalarNode)
+                        .filter(nodeTuple -> "id".equals(((ScalarNode) nodeTuple.getKeyNode()).getValue()))
+                        .findAny()
+                        .orElse(null);
+                if (idNode != null && idNode.getValueNode() instanceof ScalarNode) {
+                    String idValue = ((ScalarNode) idNode.getValueNode()).getValue();
+                    if (idValue != null) {
+                        return "objId" + idValue;
+                    }
+                }
+            }
+            return "id" + (lastAnchorId++);
+        }
     }
 }
