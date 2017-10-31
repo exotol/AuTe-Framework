7f353ceaf1b25ed1bd75585d56fc885bc4e4ead0
Valeria Bolshakova <valeria.bolshakova@bsc-ideas.com>
2017-10-31 11:36:00 +0300

    COM-27: ignore part of json

--- src/main/java/ru/bsc/test/at/executor/validation/IgnoringComparator.java
+++ src/main/java/ru/bsc/test/at/executor/validation/IgnoringComparator.java
@@ -6,16 +6,13 @@ import org.skyscreamer.jsonassert.JSONCompareMode;
 import org.skyscreamer.jsonassert.JSONCompareResult;
 import org.skyscreamer.jsonassert.comparator.DefaultComparator;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 /**
  * Created by rrudakov on 10/7/16.
  * Project name bcs-rest-at
  */
 public class IgnoringComparator extends DefaultComparator {
+
+    private static final String IGNORE = "*ignore*";
 
     public IgnoringComparator(JSONCompareMode mode) {
         super(mode);
@@ -23,12 +20,13 @@ public class IgnoringComparator extends DefaultComparator {
 
     @Override
     public void compareValues(String prefix, Object expectedValue, Object actualValue, JSONCompareResult result) throws JSONException {
-        if (!expectedValue.equals("*ignore*")) {
+        if (!expectedValue.equals(IGNORE)) {
             if (expectedValue instanceof JSONObject && actualValue instanceof JSONObject ||
                     expectedValue instanceof String && actualValue instanceof String) {
                 String expected = expectedValue.toString();
                 String actual = actualValue.toString();
-                if (!matchesPattern(expected, actual)) {
+                String[] expectedParts = expected.split("\\*ignore\\*");
+                if (!containsValues(expectedParts, actual)) {
                     result.fail(prefix, expectedValue, actualValue);
                 }
             }
@@ -38,27 +36,18 @@ public class IgnoringComparator extends DefaultComparator {
         }
     }
 
-    private boolean matchesPattern(String expected, String actual) {
-        StringBuilder regex = new StringBuilder();
-        Boolean isIgnoreEnd = expected.endsWith("*ignore*");
-        if (expected.indexOf("*ignore*") == 0) {
-            regex.append(".*");
-        }
-        List<String> exParts = Arrays.asList(expected.split("\\*ignore\\*"));
-        for (String s : exParts) {
-            regex.append(Pattern.quote(s));
-            if (exParts.indexOf(s) == (exParts.size() - 1)) {
-                if (isIgnoreEnd) {
-                    regex.append(".*");
-                }
-                else {
-                    break;
-                }
+    private boolean containsValues(String[] values, String actual) {
+        int position = actual.indexOf(IGNORE);
+        for (String value : values) {
+            if (!actual.contains(value)) {
+                return false;
             }
-            regex.append(".*");
+            int valuePos = actual.indexOf(value, position);
+            if (valuePos < position) {
+                return false;
+            }
+            position = valuePos + value.length();
         }
-        Pattern pattern = Pattern.compile(regex.toString());
-        Matcher matcher = pattern.matcher(actual);
-        return matcher.matches();
+        return true;
     }
 }
