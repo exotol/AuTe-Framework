b1f2c629118028e5f8c5777b9592bd9747b7f832
Stanislav Doroshin <stanislav.doroshin@bsc-ideas.com>
2017-10-31 17:00:46 +0300

    Назначение id при сохранении проекта (с вложенными элементами). Проверка и исправление id при чтении из yaml.

--- src/main/java/ru/bsc/test/autotester/component/ProjectsSource.java
+++ src/main/java/ru/bsc/test/autotester/component/ProjectsSource.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.yaml.snakeyaml.Yaml;
+import ru.bsc.test.at.executor.model.AbstractModel;
 import ru.bsc.test.at.executor.model.Project;
 
 import javax.annotation.PostConstruct;
@@ -12,8 +13,10 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
 /**
  * Created by sdoroshin on 27.10.2017.
@@ -48,6 +51,11 @@ public class ProjectsSource {
                 }
             }
         }
+
+        // Проверка и восстановление id:
+        // - исправляются повторения id
+        // - исправляются неназначенные id (id == null)
+        checkAndRepairId();
     }
 
     public void setDirectoryPath(String directoryPath) {
@@ -59,6 +67,7 @@ public class ProjectsSource {
     }
 
     public void save() {
+        checkAndRepairId();
         saveToFiles();
     }
 
@@ -73,5 +82,75 @@ public class ProjectsSource {
                 LOGGER.error("Save file " + directoryPath + projectItem.getProjectCode() + "/" + MAIN_YAML_FILENAME, e);
             }
         });
+    }
+
+    private void checkAndRepairId() {
+        Set<Long> idSet = new LinkedHashSet<>();
+        applyToAllModels(model -> collectAndReplaceRepeatsId(idSet, model));
+        applyToAllModels(model -> newIdToModel(idSet, model));
+    }
+
+    private void collectAndReplaceRepeatsId(Set<Long> idSet, AbstractModel model) {
+        if (idSet == null || model == null) {
+            return;
+        }
+        if (model.getId() != null) {
+            Long newId = idSet.add(model.getId()) ? model.getId() : null;
+            model.setId(newId);
+        }
+    }
+
+    private void newIdToModel(Set<Long> idSet, AbstractModel model) {
+        if (idSet == null || model == null) {
+            return;
+        }
+
+        if (model.getId() == null) {
+            Long lastId = idSet.stream().max(Long::compareTo).orElse(1L);
+            Long newId = lastId + 1;
+            idSet.add(newId);
+            model.setId(newId);
+        }
+    }
+
+    private void applyToAllModels(IMethodToModel methodToModel) {
+        projectList.forEach(project -> {
+            methodToModel.method(project);
+            if (project.getScenarios() != null) {
+                project.getScenarios().forEach(scenario -> {
+                    methodToModel.method(scenario);
+                    if (scenario.getSteps() != null) {
+                        scenario.getSteps().forEach(step -> {
+                            methodToModel.method(step);
+                            if (step.getExpectedServiceRequests() != null) {
+                                step.getExpectedServiceRequests().forEach(methodToModel::method);
+                            }
+                            if (step.getStepParameterSetList() != null) {
+                                step.getStepParameterSetList().forEach(stepParameterSet -> {
+                                    methodToModel.method(stepParameterSet);
+                                    if (stepParameterSet.getStepParameterList() != null) {
+                                        stepParameterSet.getStepParameterList().forEach(methodToModel::method);
+                                    }
+                                });
+                            }
+                            if (step.getMockServiceResponseList() != null) {
+                                step.getMockServiceResponseList().forEach(methodToModel::method);
+                            }
+                        });
+                    }
+                });
+            }
+            if (project.getScenarioGroups() != null) {
+                project.getScenarioGroups().forEach(methodToModel::method);
+            }
+            if (project.getStandList() != null) {
+                project.getStandList().forEach(methodToModel::method);
+            }
+        });
+    }
+
+    @FunctionalInterface
+    interface IMethodToModel {
+        void method(AbstractModel model);
     }
 }
