c8fa687ef9a090d71ed705e11f0b22a9131cc113
Valeria Bolshakova <valeria.bolshakova@bsc-ideas.com>
2017-11-16 17:25:03 +0300

    COM-49 Неправильное сравнение строковых полей в JSON

--- src/main/java/ru/bsc/test/at/executor/validation/MaskComparator.java
+++ src/main/java/ru/bsc/test/at/executor/validation/MaskComparator.java
@@ -1,8 +1,5 @@
 package ru.bsc.test.at.executor.validation;
 
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.List;
 import java.util.regex.Pattern;
 
 public class MaskComparator {
@@ -12,19 +9,19 @@ public class MaskComparator {
     public static boolean compare(Object expectedValue, Object actualValue) {
         String expected = expectedValue.toString();
         String actual = actualValue.toString();
-        List<String> expectedParts = Arrays.asList(expected.split(Pattern.quote(IGNORE)));
+        String[] expectedParts = expected.split(Pattern.quote(IGNORE));
         int position = 0;
-        Iterator iterator = expectedParts.iterator();
-        while (iterator.hasNext()) {
-            String value = (String) iterator.next();
+        String lastvalue = null;
+        for (String value : expectedParts) {
             int valuePos = actual.indexOf(value, position);
             if (valuePos < 0 || valuePos < position) {
                 return false;
             }
-            if (!expected.endsWith(IGNORE) && !iterator.hasNext()) {
-                return actual.substring(valuePos, actual.length()).equals(value);
-            }
             position = valuePos + value.length();
+            lastvalue = value;
+        }
+        if (!expected.endsWith(IGNORE)) {
+            return actual.endsWith(lastvalue);
         }
         return true;
     }
