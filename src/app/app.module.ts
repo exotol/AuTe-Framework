bf3c9169f2d46394fab014edc3bc9257f0de0504
Eloginov <Egor.Loginov@bsc-ideas.com>
2017-12-14 15:47:53 +0300

    COM-16: Comma in app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -50,7 +50,7 @@ const routes: Routes = [
     ProjectSettingsComponent,
     ScenarioSettingsComponent,
     DiffComponent,
-    SyncScrollDirective
+    SyncScrollDirective,
     ScenarioSettingsComponent,
     SearchComponent
   ],
