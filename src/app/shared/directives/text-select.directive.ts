dff956840731728739bfcb4ebeab5f99a9ff2b3c
Eloginov <Egor.Loginov@bsc-ideas.com>
2017-12-15 12:22:14 +0300

    com-28 refactoring

--- src/app/shared/directives/text-select.directive.ts
+++ src/app/shared/directives/text-select.directive.ts
@@ -1,18 +1,31 @@
-import {Directive, ElementRef, HostListener} from '@angular/core';
+import {Directive, ElementRef, HostListener, AfterViewInit} from '@angular/core';
 
 @Directive({
   selector: '[appTextSelect]',
 })
-export class TextSelectDirective {
+export class TextSelectDirective implements AfterViewInit {
 
-  @HostListener('keydown', ['$event']) overrideSelectAllShortcut(e) {
-    if (e.ctrlKey && e.key === 'a') {
+  content: string;
+
+  @HostListener('keydown', ['$event']) disableTextChangingEvents(e) {
+    if (e.ctrlKey && 'zxv'.includes(e.key) || [8, 46].includes(e.keyCode)) {
       e.preventDefault();
-      window.getSelection().selectAllChildren(this.ref.nativeElement);
     }
   }
 
+  @HostListener('keypress', ['$event']) disableKeypress(e) {
+    e.preventDefault();
+  }
+
+  @HostListener('input') disableInput(e) {
+    this.ref.nativeElement.innerHTML = this.content;
+  }
+
   constructor(private ref: ElementRef) {
-    this.ref.nativeElement.tabIndex = 0;
+    this.ref.nativeElement.contentEditable = true;
+  }
+
+  ngAfterViewInit() {
+    this.content = this.ref.nativeElement.innerHTML;
   }
 }
