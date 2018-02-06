da6cb3c7e27ead07c34a61db78f313442c4cc873
Stanislav Doroshin <stanislav.doroshin@bsc-ideas.com>
2018-02-06 16:42:41 +0300

    COM-84 Исправление: Циклическая отправка запросов. Счетчик проверяется после проверки ответа.

--- src/main/java/ru/bsc/test/at/executor/service/AtExecutor.java
+++ src/main/java/ru/bsc/test/at/executor/service/AtExecutor.java
@@ -243,67 +243,130 @@ public class AtExecutor {
         // 2.3 Подстановка переменных сценария в заголовки запроса
         String requestHeaders = insertSavedValues(step.getRequestHeaders(), savedValues);
 
-        int retryCounter = 0;
-        boolean retry = false;
         int numberRepetitions;
-        ResponseHelper responseData;
+        int repetitionCounter = 0;
         do {
-            retryCounter++;
-            // 3. Выполнить запрос
-            if (step.getRequestBodyType() == null || RequestBodyType.JSON.equals(step.getRequestBodyType())) {
-                responseData = http.request(
-                        step.getRequestMethod(),
-                        requestUrl,
-                        requestBody,
-                        requestHeaders,
-                        project.getTestIdHeaderName(),
-                        testId);
-            } else {
-                if (step.getFormDataList() == null) {
-                    step.setFormDataList(Collections.emptyList());
+            repetitionCounter++;
+            int retryCounter = 0;
+            boolean retry = false;
+            ResponseHelper responseData;
+            do {
+                retryCounter++;
+                // 3. Выполнить запрос
+                if (step.getRequestBodyType() == null || RequestBodyType.JSON.equals(step.getRequestBodyType())) {
+                    responseData = http.request(
+                            step.getRequestMethod(),
+                            requestUrl,
+                            requestBody,
+                            requestHeaders,
+                            project.getTestIdHeaderName(),
+                            testId);
+                } else {
+                    if (step.getFormDataList() == null) {
+                        step.setFormDataList(Collections.emptyList());
+                    }
+                    stepResult.setRequestBody(
+                            step.getFormDataList()
+                                    .stream()
+                                    .map(formData -> {
+                                        String result = formData.getFieldName() + " = ";
+                                        if (FieldType.TEXT.equals(formData.getFieldType()) || formData.getFieldType() == null) {
+                                            result += formData.getValue();
+                                        } else {
+                                            result += (projectPath == null ? "" : projectPath) + formData.getFilePath();
+                                        }
+                                        return result;
+                                    })
+                                    .collect(Collectors.joining("\r\n")));
+                    responseData = http.request(
+                            step.getRequestMethod(),
+                            projectPath,
+                            requestUrl,
+                            step.getMultipartFormData(),
+                            step.getFormDataList(),
+                            requestHeaders,
+                            project.getTestIdHeaderName(),
+                            testId);
                 }
-                stepResult.setRequestBody(
-                        step.getFormDataList()
-                                .stream()
-                                .map(formData -> {
-                                    String result = formData.getFieldName() + " = ";
-                                    if (FieldType.TEXT.equals(formData.getFieldType()) || formData.getFieldType() == null) {
-                                        result += formData.getValue();
-                                    } else {
-                                        result += (projectPath == null ? "" : projectPath) + formData.getFilePath();
-                                    }
-                                    return result;
-                                })
-                                .collect(Collectors.joining("\r\n")));
-                responseData = http.request(
-                        step.getRequestMethod(),
-                        projectPath,
-                        requestUrl,
-                        step.getMultipartFormData(),
-                        step.getFormDataList(),
-                        requestHeaders,
-                        project.getTestIdHeaderName(),
-                        testId);
+
+                // Выполнить скрипт
+                if (StringUtils.isNotEmpty(step.getScript())) {
+                    ScriptEngine scriptEngine = new ScriptEngineManager().getEngineByName("js");
+                    scriptEngine.put("stepStatus", new StepStatus());
+                    scriptEngine.put("scenarioVariables", savedValues);
+                    scriptEngine.put("response", responseData);
+
+                    scriptEngine.eval(step.getScript());
+
+                    StepStatus stepStatus = (StepStatus) scriptEngine.get("stepStatus");
+                    if (StringUtils.isNotEmpty(stepStatus.getException())) {
+                        throw new Exception(stepStatus.getException());
+                    }
+                }
+
+                // 3.1. Polling
+                if (step.getUsePolling()) {
+                    retry = tryUsePolling(step, responseData);
+                }
+            } while (retry && retryCounter <= POLLING_RETRY_COUNT);
+
+            stepResult.setPollingRetryCount(retryCounter);
+            stepResult.setActual(responseData.getContent());
+            stepResult.setExpected(step.getExpectedResponse());
+
+            // 4. Сохранить полученные значения
+            saveValuesByJsonXPath(step, responseData, savedValues);
+
+            stepResult.setSavedParameters(savedValues.toString());
+
+            // 4.1 Проверить сохраненные значения
+            if (step.getSavedValuesCheck() != null) {
+                for (Map.Entry<String, String> entry : step.getSavedValuesCheck().entrySet()) {
+                    String valueExpected = entry.getValue() == null ? "" : entry.getValue();
+                    for (Map.Entry<String, String> savedVal : savedValues.entrySet()) {
+                        String key = String.format("%%%s%%", savedVal.getKey());
+                        valueExpected = valueExpected.replaceAll(key, savedVal.getValue());
+                    }
+                    String valueActual = savedValues.get(entry.getKey());
+                    if (!valueExpected.equals(valueActual)) {
+                        throw new Exception("Saved value " + entry.getKey() + " = " + valueActual + ". Expected: " + valueExpected);
+                    }
+                }
             }
 
-            // Выполнить скрипт
-            if (StringUtils.isNotEmpty(step.getScript())) {
-                ScriptEngine scriptEngine = new ScriptEngineManager().getEngineByName("js");
-                scriptEngine.put("stepStatus", new StepStatus());
-                scriptEngine.put("scenarioVariables", savedValues);
-                scriptEngine.put("response", responseData);
+            // 5. Подставить сохраненые значения в ожидаемый результат
+            String expectedResponse = insertSavedValues(step.getExpectedResponse(), savedValues);
+            // 5.1. Расчитать выражения <f></f>
+            expectedResponse = evaluateExpressions(expectedResponse);
+            stepResult.setExpected(expectedResponse);
 
-                scriptEngine.eval(step.getScript());
+            // 6. Проверить код статуса ответа
+            if ((step.getExpectedStatusCode() != null)
+                    && (step.getExpectedStatusCode() != responseData.getStatusCode())) {
+                throw new Exception("Expected status code: " + step.getExpectedStatusCode() + ". Actual status code: " + responseData.getStatusCode());
 
-                StepStatus stepStatus = (StepStatus) scriptEngine.get("stepStatus");
-                if (StringUtils.isNotEmpty(stepStatus.getException())) {
-                    throw new Exception(stepStatus.getException());
-                }
             }
 
-            // 3.1. Polling
-            if (step.getUsePolling()) {
-                retry = tryUsePolling(step, responseData);
+            if (!step.getExpectedResponseIgnore()) {
+                if (step.getResponseCompareMode() == null) {
+                    JSONComparing(expectedResponse, responseData, step.getJsonCompareMode());
+                } else {
+                    switch (step.getResponseCompareMode()) {
+                        case FULL_MATCH:
+                            if (!StringUtils.equals(expectedResponse, responseData.getContent())) {
+                                throw new Exception("\nExpected value: " + expectedResponse + ".\nActual value: " + responseData.getContent());
+                            }
+                            break;
+                        case IGNORE_MASK:
+                            if (!MaskComparator.compare(expectedResponse, responseData.getContent())) {
+                                throw new Exception("\nExpected value: " + expectedResponse + ".\nActual value: " + responseData.getContent());
+                            }
+                            break;
+                        default:
+                            JSONComparing(expectedResponse, responseData, step.getJsonCompareMode());
+                            break;
+                    }
+                }
             }
 
             // 3.2. Cyclic sending request, COM-84
@@ -316,67 +379,7 @@ public class AtExecutor {
                     numberRepetitions = 1;
                 }
             }
-
-        } while ((retry && retryCounter <= POLLING_RETRY_COUNT) || (retryCounter < numberRepetitions ) );
-
-        stepResult.setPollingRetryCount(retryCounter);
-        stepResult.setActual(responseData.getContent());
-        stepResult.setExpected(step.getExpectedResponse());
-
-        // 4. Сохранить полученные значения
-        saveValuesByJsonXPath(step, responseData, savedValues);
-
-        stepResult.setSavedParameters(savedValues.toString());
-
-        // 4.1 Проверить сохраненные значения
-        if (step.getSavedValuesCheck() != null) {
-            for (Map.Entry<String, String> entry : step.getSavedValuesCheck().entrySet()) {
-                String valueExpected = entry.getValue() == null ? "" : entry.getValue();
-                for (Map.Entry<String, String> savedVal : savedValues.entrySet()) {
-                    String key = String.format("%%%s%%", savedVal.getKey());
-                    valueExpected = valueExpected.replaceAll(key, savedVal.getValue());
-                }
-                String valueActual = savedValues.get(entry.getKey());
-                if (!valueExpected.equals(valueActual)) {
-                    throw new Exception("Saved value " + entry.getKey() + " = " + valueActual + ". Expected: " + valueExpected);
-                }
-            }
-        }
-
-        // 5. Подставить сохраненые значения в ожидаемый результат
-        String expectedResponse = insertSavedValues(step.getExpectedResponse(), savedValues);
-        // 5.1. Расчитать выражения <f></f>
-        expectedResponse = evaluateExpressions(expectedResponse);
-        stepResult.setExpected(expectedResponse);
-
-        // 6. Проверить код статуса ответа
-        if ((step.getExpectedStatusCode() != null)
-                && (step.getExpectedStatusCode() != responseData.getStatusCode())) {
-            throw new Exception("Expected status code: " + step.getExpectedStatusCode() + ". Actual status code: " + responseData.getStatusCode());
-
-        }
-
-        if (!step.getExpectedResponseIgnore()) {
-            if (step.getResponseCompareMode() == null) {
-                JSONComparing(expectedResponse, responseData, step.getJsonCompareMode());
-            } else {
-                switch (step.getResponseCompareMode()) {
-                    case FULL_MATCH:
-                        if (!StringUtils.equals(expectedResponse, responseData.getContent())) {
-                            throw new Exception("\nExpected value: " + expectedResponse + ".\nActual value: " + responseData.getContent());
-                        }
-                        break;
-                    case IGNORE_MASK:
-                        if (!MaskComparator.compare(expectedResponse, responseData.getContent())) {
-                            throw new Exception("\nExpected value: " + expectedResponse + ".\nActual value: " + responseData.getContent());
-                        }
-                        break;
-                    default:
-                        JSONComparing(expectedResponse, responseData, step.getJsonCompareMode());
-                        break;
-                }
-            }
-        }
+        } while (repetitionCounter < numberRepetitions);
     }
 
     private void JSONComparing(String expectedResponse, ResponseHelper responseData, String jsonCompareMode) throws Exception {
