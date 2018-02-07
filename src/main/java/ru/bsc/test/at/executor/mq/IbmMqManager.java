18ef3cf95a3c277e1e7f7dcdd5d044f646c005b3
Михаил Курцман <Kurtsman.MG@nsd.ru>
2018-02-07 15:16:13 +0300

    COM-108 Доработка тестирования MQ (удалил коментарии)

--- src/main/java/ru/bsc/test/at/executor/mq/IbmMqManager.java
+++ src/main/java/ru/bsc/test/at/executor/mq/IbmMqManager.java
@@ -1,24 +1,14 @@
 package ru.bsc.test.at.executor.mq;
 
 
+import com.ibm.mq.jms.*;
+import com.ibm.msg.client.wmq.WMQConstants;
+
 import javax.jms.Session;
 import javax.jms.TextMessage;
 
-import com.ibm.mq.jms.MQConnectionFactory;
-import com.ibm.mq.jms.MQQueue;
-import com.ibm.mq.jms.MQQueueConnection;
-import com.ibm.mq.jms.MQQueueConnectionFactory;
-import com.ibm.mq.jms.MQQueueSender;
-import com.ibm.mq.jms.MQQueueSession;
-import com.ibm.msg.client.wmq.WMQConstants;
-
 
 public class IbmMqManager implements IMqManager {
-
-
-    //    public static final String JNDI_PROVIDER = "file:/C:/tmp/jms/";
-    //    public static final String CONNECTION_FACTORY = "com.sun.jndi.fscontext.RefFSContextFactory";
-    //    public static final String JNDI_ADDRESS = "connectionfactory";
 
     private MQConnectionFactory connectionFactory = new MQQueueConnectionFactory();
 
@@ -26,25 +16,6 @@ public class IbmMqManager implements IMqManager {
     private String password;
     private String host;
     private int port;
-    private int ccid;
-    private String channel;
-    private String queueManager;
-    private int transportType;
-
-
-    //    public IbmMqManager() throws Exception {
-    //        try {
-    //                        java.util.Hashtable environment = new java.util.Hashtable();
-    //                        environment.put(Context.PROVIDER_URL, JNDI_PROVIDER);
-    //                        environment.put(Context.INITIAL_CONTEXT_FACTORY, CONNECTION_FACTORY);
-    //                        Context ctx = new InitialDirContext(environment);
-    //                        connectionFactory = (MQConnectionFactory) ctx.lookup(JNDI_ADDRESS);
-    //
-    //        } catch(Exception ex) {
-    //            throw new RuntimeException(ex);
-    //        }
-    //
-    //    }
 
     @Override
     public void setHost(String host) {
