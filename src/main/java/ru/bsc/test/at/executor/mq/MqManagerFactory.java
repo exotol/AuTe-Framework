9b63dd72a6387ef867ed7fe8b36872d23e8a852a
Stanislav Doroshin <stanislav.doroshin@bsc-ideas.com>
2018-02-09 15:12:36 +0300

    COM-108 IBM MQ Незначительные правки кода.

--- src/main/java/ru/bsc/test/at/executor/mq/MqManagerFactory.java
+++ src/main/java/ru/bsc/test/at/executor/mq/MqManagerFactory.java
@@ -2,12 +2,12 @@ package ru.bsc.test.at.executor.mq;
 
 public class MqManagerFactory {
 
-    public static IMqManager getMqManager(MqService mqService){
+    public static IMqManager getMqManager(MqService mqService) {
         if (MqService.ACTIVE_MQ.equals(mqService)) {
             return new ActiveMqManager();
         } else if (MqService.RABBIT_MQ.equals(mqService)) {
             return new RabbitMqManager();
-        } else if(MqService.IBM_MQ.equals(mqService)){
+        } else if(MqService.IBM_MQ.equals(mqService)) {
             return new IbmMqManager();
         } else {
             throw new UnsupportedOperationException("MqService " + mqService + " is not supported");
