0d9592be52e186f0efe09fc869ec97b936132a39
Макаров Сергей <sergey.makarov@bsc-ideas.com>
2018-03-05 09:31:38 +0300

    оптимизация импортов в файле GroovyDirective.java

--- src/main/java/ru/bsc/wiremock/velocity/directive/GroovyDirective.java
+++ src/main/java/ru/bsc/wiremock/velocity/directive/GroovyDirective.java
@@ -1,15 +1,12 @@
 package ru.bsc.wiremock.velocity.directive;
 
-import java.io.Writer;
-
 import groovy.lang.Binding;
 import groovy.lang.GroovyShell;
 import org.apache.velocity.context.InternalContextAdapter;
-import org.apache.velocity.exception.MethodInvocationException;
-import org.apache.velocity.exception.ParseErrorException;
-import org.apache.velocity.exception.ResourceNotFoundException;
 import org.apache.velocity.runtime.directive.Directive;
 import org.apache.velocity.runtime.parser.node.Node;
+
+import java.io.Writer;
 
 /**
  * Created by sdoroshin on 14.08.2017.
