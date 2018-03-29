1e6824fdf54f3d9e7ecba94da6557c2c5f69f078
Макаров Сергей <sergey.makarov@bsc-ideas.com>
2018-03-29 14:00:49 +0300

    COM-92 - Рассмотреть применимость Allure для отчетности, доработка по замечаниям

--- src/main/java/ru/bsc/test/autotester/report/impl/allure/attach/extract/impl/RequestAttachExtractor.java
+++ src/main/java/ru/bsc/test/autotester/report/impl/allure/attach/extract/impl/RequestAttachExtractor.java
@@ -2,11 +2,14 @@ package ru.bsc.test.autotester.report.impl.allure.attach.extract.impl;
 
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
+import ru.bsc.test.at.executor.model.FieldType;
+import ru.bsc.test.at.executor.model.RequestBodyType;
 import ru.bsc.test.at.executor.model.Step;
 import ru.bsc.test.at.executor.model.StepResult;
 import ru.yandex.qatools.allure.model.Attachment;
 
 import java.io.File;
+import java.util.stream.Collectors;
 
 /**
  * Created by smakarov
@@ -19,6 +22,9 @@ public class RequestAttachExtractor extends AbstractAttachExtractor {
 
     @Override
     public Attachment extract(File resultDirectory, StepResult result) {
+        if (StringUtils.isEmpty(result.getStep().getRelativeUrl())) {
+            return null;
+        }
         String requestData = getRequestData(result);
         String relativePath = writeDataToFile(resultDirectory, requestData, FILE_NAME);
         if (relativePath != null) {
@@ -40,6 +46,19 @@ public class RequestAttachExtractor extends AbstractAttachExtractor {
         }
         if (step.getExpectedStatusCode() != null) {
             data += "\nExpected status: " + step.getExpectedStatusCode();
+        }
+        if (step.getRequestBodyType() == null || step.getRequestBodyType() == RequestBodyType.JSON) {
+            data += "\nRequest body [json data]:\n";
+            data += step.getRequest();
+        } else {
+            data += "\nRequest body " + (step.getMultipartFormData() ? "[multipart/form-data]" : "[form-data]") + "\n";
+            data += step.getFormDataList().stream()
+                    .map(formData ->
+                            formData.getFieldName() +
+                            (formData.getFieldType() == FieldType.TEXT ?
+                             " = " + formData.getValue() :
+                             "path: " + formData.getFilePath() + ", mime type: " + formData.getMimeType()))
+                    .collect(Collectors.joining("\n"));
         }
         return data;
     }
