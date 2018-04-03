71bfed6f932f0a5243a94fcd5d5e362acd0c98ea
Михаил Курцман <Kurtsman.MG@nsd.ru>
2018-04-03 12:03:58 +0300

    COM-151 Очень долго загружаются детали шага после прогона сценария

--- src/main/java/ru/bsc/test/autotester/controller/rest/RestExecutionController.java
+++ src/main/java/ru/bsc/test/autotester/controller/rest/RestExecutionController.java
@@ -1,22 +1,16 @@
 package ru.bsc.test.autotester.controller.rest;
 
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import ru.bsc.test.autotester.diff.Diff;
-import ru.bsc.test.autotester.diff.DiffMatchPatch;
 import ru.bsc.test.autotester.mapper.ExecutionResultRoMapper;
 import ru.bsc.test.autotester.ro.ExecutionResultRo;
 import ru.bsc.test.autotester.ro.MultipleReportsRequestRo;
 import ru.bsc.test.autotester.service.ScenarioService;
 
 import javax.servlet.http.HttpServletResponse;
-import java.util.LinkedList;
 import java.util.zip.ZipOutputStream;
 
 /**
@@ -46,42 +40,7 @@ public class RestExecutionController {
 
     @RequestMapping(value = "{executionUuid}/status", method = RequestMethod.GET)
     public ExecutionResultRo getStatus(@PathVariable String executionUuid) {
-        ExecutionResultRo map = executionResultRoMapper.map(scenarioService.getResult(executionUuid));
-        return addDiffToResult(map);
-    }
-
-    private ExecutionResultRo addDiffToResult(ExecutionResultRo map) {
-         map.getScenarioResultList().forEach(rl -> rl.getStepResultList().forEach( stepResultRo -> {
-            if(stepResultRo.getActual() == null || stepResultRo.getExpected() == null){
-                return;
-            }
-
-             DiffMatchPatch dmp = new DiffMatchPatch();
-             Gson gson = new GsonBuilder().setPrettyPrinting().create();
-             JsonParser parser = new JsonParser();
-             JsonElement elA;
-             try {
-                 elA = parser.parse(stepResultRo.getActual());
-                 String actualJson = gson.toJson(elA);
-                 stepResultRo.setActual(actualJson);
-             }catch (Exception e){}
-
-             JsonElement elE;
-
-             try {
-                 elE = parser.parse(stepResultRo.getExpected());
-                 String expectedJson = gson.toJson(elE);
-                 stepResultRo.setExpected(expectedJson);
-             }catch (Exception e){}
-
-             LinkedList<Diff> diff = dmp.diff_main(stepResultRo.getExpected(), stepResultRo.getActual());
-             stepResultRo.setDiff(diff);
-
-        }));
-
-
-
-        return map;
+        return executionResultRoMapper.map(scenarioService.getResult(executionUuid));
     }
 
     @RequestMapping(value = "{executionUuid}/report", method = RequestMethod.GET, produces="application/zip")
