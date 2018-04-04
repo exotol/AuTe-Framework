77255e219b1896261ef8051d98ae335f09a81363
Stanislav Doroshin <stanislav.doroshin@bsc-ideas.com>
2018-04-04 12:56:06 +0300

    Обновлена версия в run.bat и readme.md.

--- src/main/resources/run.bat
+++ src/main/resources/run.bat
@@ -1,1 +1,1 @@
-java -Dloader.path=lib/ -Dfile.encoding=UTF-8 -jar autotester-3.0.0.jar --server.port=8080
+java -Dloader.path=lib/ -Dfile.encoding=UTF-8 -jar autotester-4.0.0.jar --server.port=8080
