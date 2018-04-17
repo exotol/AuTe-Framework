3b95c3dbfc332fcfb480f912cd77a67d5f0ca616
Stanislav Doroshin <stanislav.doroshin@bsc-ideas.com>
2018-04-17 14:37:39 +0300

    COM-119 Реализация не только отправки, но и ожидания сообщений для RabbitMQ.

--- src/main/java/ru/bsc/test/at/executor/mq/RabbitMqManager.java
+++ src/main/java/ru/bsc/test/at/executor/mq/RabbitMqManager.java
@@ -1,38 +1,113 @@
 package ru.bsc.test.at.executor.mq;
 
-import com.rabbitmq.jms.admin.RMQConnectionFactory;
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.QueueingConsumer;
+import com.rabbitmq.jms.client.message.RMQTextMessage;
 import lombok.extern.slf4j.Slf4j;
 
 import javax.jms.Connection;
 import javax.jms.JMSException;
+import javax.jms.Message;
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 class RabbitMqManager extends AbstractMqManager {
 
-    private final Connection connection;
+    private final com.rabbitmq.client.Connection senderConnection;
 
     RabbitMqManager(String host, int port, String username, String password) throws JMSException {
-        RMQConnectionFactory cf = new RMQConnectionFactory();
-        cf.setHost(host);
-        cf.setPort(port);
-        cf.setUsername(username);
-        cf.setPassword(password);
-        connection = cf.createConnection();
-        connection.start();
+
+        try {
+            // Это костыль для отправки сообщений в RabbitMQ.
+            // Сообщения не отправляются в очередь через стандартные интерфейсы JMS, если у очереди указан параметр x-dead-letter-exchange
+            ConnectionFactory connectionFactory = new ConnectionFactory();
+            connectionFactory.setHost(host);
+            connectionFactory.setPort(port);
+            connectionFactory.setUsername(username);
+            connectionFactory.setPassword(password);
+            senderConnection = connectionFactory.newConnection();
+        } catch (Exception e) {
+            log.error("{}", e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void sendTextMessage(String queueName, String message, Map<String, Object> properties, String testIdHeaderName, String testId) throws Exception {
+        Channel channel = senderConnection.createChannel();
+
+        AMQP.BasicProperties.Builder propertiesBuilder = new AMQP.BasicProperties().builder();
+
+        Map<String, Object> headers = new HashMap<>();
+
+        if (properties != null) {
+            properties.forEach((name, value) -> {
+                String stringValue = value instanceof String ? (String) value : null;
+                if ("messageId".equals(name)) {
+                    propertiesBuilder.messageId(stringValue);
+                } else if ("contentType".equals(name)) {
+                    propertiesBuilder.contentType(stringValue);
+                } else if ("contentEncoding".equals(name)) {
+                    propertiesBuilder.contentEncoding(stringValue);
+                } else if ("correlationId".equals(name)) {
+                    propertiesBuilder.correlationId(stringValue);
+                } else if ("replyTo".equals(name)) {
+                    propertiesBuilder.replyTo(stringValue);
+                } else if ("timestamp".equals(name)) {
+                    try {
+                        DateFormat formatter = new SimpleDateFormat("dd-MM-yyyy,HH:mm:ss SSS");
+                        propertiesBuilder.timestamp(formatter.parse(stringValue));
+                    } catch (ParseException e) {
+                        log.error("{}", e);
+                    }
+                } else {
+                    headers.put(name, value);
+                }
+            });
+        }
+
+        channel.basicPublish("", queueName, propertiesBuilder.headers(headers).build(), message.getBytes());
+        channel.close();
     }
 
     @Override
     Connection getConnection() {
-        return connection;
+        throw new UnsupportedOperationException("getConnection unsupported in RabbitMqManager. RabbitMQ using custom implementation.");
     }
 
     @Override
     public void close() throws IOException {
+        senderConnection.close();
+    }
+
+    @Override
+    public Message waitMessage(String queueName, Long timeoutMs, String testIdHeaderName, String testId) throws JMSException {
         try {
-            connection.close();
-        } catch (JMSException e) {
-            log.error("{}", e);
+            Channel channel = senderConnection.createChannel();
+            // channel.queueBind(queueName, "", queueName);
+
+            final QueueingConsumer consumer = new QueueingConsumer(channel);
+            channel.basicConsume(queueName, true, consumer);
+
+            QueueingConsumer.Delivery delivery = consumer.nextDelivery(timeoutMs);
+            if (delivery != null) {
+
+                channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+                RMQTextMessage message = new RMQTextMessage();
+                message.setText(new String(delivery.getBody()));
+                return message;
+            }
+            return null;
+        } catch (IOException | InterruptedException e) {
+            log.error("RabbitMQ waitMessage error: {}", e);
         }
+        return null;
     }
 }
