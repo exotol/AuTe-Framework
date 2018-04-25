e2104cbaf3b41647dbc75027b28272e1721edd2d
Михаил Курцман <Kurtsman.MG@nsd.ru>
2018-04-25 14:40:01 +0300

    COM-172 Добавить возможность создавать новый проект через гуи

--- src/app/project-list/project-list.component.ts
+++ src/app/project-list/project-list.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {Project} from '../model/project';
 import {ProjectService} from '../service/project.service';
+import {Scenario} from '../model/scenario';
+import {CustomToastyService} from '../service/custom-toasty.service';
+import {TranslateService} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-project-list',
@@ -8,15 +11,43 @@ import {ProjectService} from '../service/project.service';
 })
 export class ProjectListComponent implements OnInit {
 
+  @Input()
+  newProjectName: string;
+
+  @Input()
+  newProjectCode: string;
+
   projectList: Project[];
-  displayImportProjectForm = false;
 
   constructor(
-    private projectService: ProjectService
+    private customToastyService: CustomToastyService,
+    private projectService: ProjectService,
+    private translate: TranslateService
   ) { }
 
   ngOnInit() {
     this.projectService.findAll().subscribe(value => this.projectList = value);
   }
 
+  saveNewProject() {
+    const newProject = new Project();
+    newProject.name = this.newProjectName;
+    newProject.code = this.newProjectCode;
+
+    const toasty = this.customToastyService.saving('Сохранение проекта...', 'Сохранение может занять некоторое время...');
+    const t = this;
+    this.projectService.save(newProject)
+      .subscribe(savedProject => {
+        t.projectList.push(savedProject);
+        t.customToastyService.success('Сохранено', 'Проект создан');
+      }, error => this.handleError(error), () => this.customToastyService.clear(toasty));
+  }
+
+  private handleError(error: any) {
+    const message = JSON.parse(error._body).message;
+    console.log(message);
+    this.translate.get(message).subscribe(value => {
+      this.customToastyService.error('Ошибка', value);
+    });
+  }
 }
