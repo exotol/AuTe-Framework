8a0858c41ea13340d8f681c659a0e8d907a0aa23
Михаил Курцман <Kurtsman.MG@nsd.ru>
2018-05-11 11:55:51 +0300

    COM-232 удалил загрузку тестовых данных

--- src/main/java/ru/bsc/test/at/mock/mq/components/ApiController.java
+++ src/main/java/ru/bsc/test/at/mock/mq/components/ApiController.java
@@ -6,23 +6,13 @@ import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import org.apache.commons.collections.Buffer;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.bsc.test.at.mock.mq.models.MockMessage;
-import ru.bsc.test.at.mock.mq.models.MockedRequest;
 
 import java.io.IOException;
 import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.UUID;
 import java.util.concurrent.TimeoutException;
 
 @RestController
@@ -36,12 +26,6 @@ public class ApiController {
     @Autowired
     public ApiController(MqRunnerComponent mqRunnerComponent) {
         this.mqRunnerComponent = mqRunnerComponent;
-        for(int i = 0; i < 1000; i++) {
-
-            MockedRequest mockedRequest = new MockedRequest();
-            mockedRequest.setMappingGuid(UUID.randomUUID().toString());
-            this.mqRunnerComponent.getFifo().add(mockedRequest);
-        }
     }
 
     @ApiOperation(value = "MQ mapping creation", notes = "Creates new mapping for MQ mock", tags = "MqMock")
