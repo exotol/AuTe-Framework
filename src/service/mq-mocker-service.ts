d81b1b2aae718b42dc4a44c92588861b6c542301
Михаил Курцман <Kurtsman.MG@nsd.ru>
2018-05-15 11:18:39 +0300

    COM-232
    Убрать ограничение на логирование 32-х последних запросов-ответов для MQ Mock

--- src/service/mq-mocker-service.ts
+++ src/service/mq-mocker-service.ts
@@ -12,7 +12,7 @@ export class MqMockerService {
 
   getRequestList(limit): Observable<MqLogItem[]> {
     return this.http
-      .get(limit ? this.mqMockerAdminUrl + '/request-list?limit=' : this.mqMockerAdminUrl + '/request-list')
+      .get(this.mqMockerAdminUrl + '/request-list' + (limit ? '?limit=' + limit : ''))
       .map(value => value.json() as MqLogItem[])
       .map(value => value.reverse());
   }
