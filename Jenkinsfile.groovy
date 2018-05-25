5b7f94047bbfc77dd24fd79a892ccba15e25856a
Макаров Сергей <sergey.makarov@bsc-ideas.com>
2018-05-25 14:39:53 +0300

    COM-242 - Слияние git репозиториев, сборка с установкой npm зависимостей

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -6,7 +6,7 @@ node() {
 		env.JAVA_HOME = tool "ORACLE JDK1.8"
 		def mvnHome = tool 'Maven 3.3.9'
 		try {
-			sh "${mvnHome}/bin/mvn clean package"
+			sh "${mvnHome}/bin/mvn clean package -P npm-install"
 		} finally {
 			junit 'target/surefire-reports/**/*.xml'
 		}
