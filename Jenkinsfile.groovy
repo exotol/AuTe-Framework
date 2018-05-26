ded54cf1975d61d85cc7ef3d037a53d33d3fbf6b
pgolovkin <pavel.golovkin@bsc-ideas.com>
2018-05-26 17:27:07 +0300

    COM-243 Изменение сборки проекта. Добавление build description

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -6,6 +6,9 @@ node() {
 		env.JAVA_HOME = tool "ORACLE JDK1.8"
 		def mvnHome = tool 'Maven 3.3.9'
 		try {
+			def shortCommit = sh(returnStdout: true, script: "git log -n 1 --pretty=format:'%h'").trim()
+			def commitCount = sh(returnStdout: true, script: "git rev-list HEAD --count").trim()
+			currentBuild.description = "${BRANCH_NAME}.${commitCount}.${shortCommit}"
 			sh "${mvnHome}/bin/mvn clean package -P npm-install"
 		} catch (exception) {
 			emailext(
